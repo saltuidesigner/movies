a73b5cf3937a1cfb9fbb70a2a0ecfd1cd90c2ab6
DESKTOP-VVO47QR\muoiii <phamweiwei300495@gmail.com>
2022-06-08 17:32:33 +0700

    Da cai redux toolkit, react-router@5.3.3, react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Switch>
+					<Route
+						path={""}
+						component={(prop: any) => {
+							return <div className='container'>123</div>;
+						}}
+					/>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
