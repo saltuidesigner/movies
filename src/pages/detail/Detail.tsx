cd018bf505e625154a471a1ff0105c3f19acd5c7
KienQuan <102026057+KienQuan@users.noreply.github.com>
2022-07-12 23:18:58 +0700

    Version_5

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -1,23 +1,74 @@
-import React from 'react'
+import '../../assets/styles/circle.scss';
+import type { RadioChangeEvent } from 'antd';
+import { Radio, Space, Tabs } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../redux/configStore';
+import { QuanLyPhimReducer } from '../../redux/reducers/TrangChu/QuanLyPhimReducer';
+import { layThongTinChiTietPhim } from '../../redux/actions/QuanLyRapAction';
 
-type Props = {}
+const { TabPane } = Tabs;
 
-export default function Detail({}: Props) {
-  return (
-    <div style={{ backgroundImage:'url(https://picsum.photos/1000)', minHeight:'100vh' }}>
+type TabPosition = 'left' | 'right' | 'top' | 'bottom';
 
-        <div className='grid grid-cols-12'>
-            <div className='col-span-4 col-start-4'>
-                <div className='grid grid-cols-2'>
-                    <img src='https://picsum.photos/200/350' alt='123' />
-                    <div>
-                        <p>Tên phim</p>
-                        <p>Mô tả</p>
+
+type Props = {
+    match: any;
+    QuanLyPhimReducer:any,
+    filmDetail: any,
+}
+
+export default function Detail(props: Props) {
+
+    const {filmDetail} = useSelector((state:RootState) =>state.QuanLyPhimReducer);
+
+    const dispatch = useDispatch();
+
+    useEffect(() =>{
+        let {id} = props.match.params;
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        (_dispatch:any)=>(layThongTinChiTietPhim(id))
+    },[])
+
+    return (
+        <div style={{ backgroundImage: `url(${filmDetail.hinhAnh})`, minHeight: '100vh' }}>
+
+            <div className='grid grid-cols-12'>
+                <div className='col-span-4 col-start-4'>
+                    <div className='grid grid-cols-2'>
+                        <img src='https://picsum.photos/200/350' alt='123' />
+                        <div>
+                            <p>Tên phim</p>
+                            <p>Mô tả</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-span-4'>
+                    <div className='c100 p50 big'>
+                        <span>99%</span>
+                        <div className="slice">
+                            <div className="bar"></div>
+                            <div className="fill"></div>
+                        </div>
                     </div>
                 </div>
             </div>
+            
+            <div className='mt-20 container'>
+            <Tabs tabPosition={'left'}>
+                <TabPane tab="Tab 1" key="1">
+                    Content of Tab 1
+                </TabPane>
+                <TabPane tab="Tab 2" key="2">
+                    Content of Tab 2
+                </TabPane>
+                <TabPane tab="Tab 3" key="3">
+                    Content of Tab 3
+                </TabPane>
+            </Tabs>
+            </div>
+            
         </div>
-        
-    </div>
-  )
+    )
 }
--- src/redux/reducers/TrangChu/QuanLyPhimReducer.tsx
+++ src/redux/reducers/TrangChu/QuanLyPhimReducer.tsx
@@ -1,5 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { baseService } from "../../../Services/baseService";
+import { SET_DANH_SACH_PHIM, SET_FILM_SAP_CHIEU,SET_FILM_DANG_CHIEU, SET_THONG_TIN_PHIM } from "../../actions/types/QuanLyPhimType"
+import { SET_CHI_TIET_PHIM } from "../../actions/types/QuanLyRapType";
 
 const initialState = {
 	arrFilm: [],
@@ -7,6 +9,48 @@ const initialState = {
 	sapChieu: false,
 	arrFilmDefault: [],
 };
+
+const stateDefault = {
+    arrFilm: [
+        {
+          "maPhim": 1282,
+          "tenPhim": "Ban tay diet quy",
+          "biDanh": "ban-tay-diet-quy",
+          "trailer": "https://www.youtube.com/embed/uqJ9u7GSaYM",
+          "hinhAnh": "http://movieapi.cyberlearn.vn/hinhanh/ban-tay-diet-quy.png",
+          "moTa": "Newlywed couple Ted and Tami-Lynn want to have a baby, but in order to qualify to be a parent, Ted will have to prove he's a person in a court of law.",
+          "maNhom": "GP00",
+          "ngayKhoiChieu": "2019-07-29T00:00:00",
+          "danhGia": 5,
+          "hot": true,
+          "dangChieu": false,
+          "sapChieu": true
+        },
+        {
+            "maPhim": 1282,
+            "tenPhim": "Ban tay diet quy",
+            "biDanh": "ban-tay-diet-quy",
+            "trailer": "https://www.youtube.com/embed/uqJ9u7GSaYM",
+            "hinhAnh": "http://movieapi.cyberlearn.vn/hinhanh/ban-tay-diet-quy.png",
+            "moTa": "Newlywed couple Ted and Tami-Lynn want to have a baby, but in order to qualify to be a parent, Ted will have to prove he's a person in a court of law.",
+            "maNhom": "GP00",
+            "ngayKhoiChieu": "2019-07-29T00:00:00",
+            "danhGia": 5,
+            "hot": true,
+            "dangChieu": false,
+            "sapChieu": true
+          }
+    ],
+    dangChieu: true,
+    sapChieu:true,
+    arrFilmDefault: [],
+    filmDetail:{},
+
+    thongTinPhim:{}
+
+
+
+}
 
 export const filmSlice = createSlice({
 	name: "film",
@@ -53,3 +97,41 @@ export const fetchFilmApi = createAsyncThunk("film/fetchFilmApi", async () => {
 		console.log(error.msg);
 	}
 });
+
+
+// export const QuanLyPhimReducer = (state=stateDefault,action ) => {
+//     switch(action.type) {
+
+//         case SET_DANH_SACH_PHIM : {
+//             state.arrFilm = action.arrFilm;
+//             state.arrFilmDefault = state.arrFilm;
+//             return {...state}
+//         }
+//         case SET_FILM_DANG_CHIEU: {
+//             state.dangChieu = !state.dangChieu;
+
+//             state.arrFilm = state.arrFilmDefault.filter(film => film.dangChieu === state.dangChieu );
+//             return {...state}
+//         }
+//         case SET_FILM_SAP_CHIEU : {
+//             state.sapChieu = !state.sapChieu;
+
+//             state.arrFilm = state.arrFilmDefault.filter(film => film.sapChieu === state.sapChieu );
+//             return {...state}
+//         }
+
+//         case SET_CHI_TIET_PHIM :{
+//             state.filmDetail = action.filmDetail;
+//             return {...state};
+//         }
+
+//         case SET_THONG_TIN_PHIM: {
+//             state.thongTinPhim = action.thongTinPhim;
+//             return {...state}
+//         }
+
+
+
+//         default : return {...state}
+//     }
+// }
